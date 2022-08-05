3ae492b6fccd2754b4f576ea129abdf2b7ce366e
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-08-05 10:10:20 -0500

    studio 8/5

--- src/Studio/areaofacircle/Area.java
+++ src/Studio/areaofacircle/Area.java
@@ -1,13 +1,29 @@
 package Studio.areaofacircle;
 
 import java.util.Scanner;
+import Studio.areaofacircle.Circle;
 
 public class Area {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         double radius = input.nextDouble();
-        double area = 3.14 * radius * radius;
-        System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+        while (radius <= 0) {
+            System.out.println("Please enter a positive integer.");
+            System.out.println("Enter a radius: ");
+            radius = input.nextDouble();
+        }
+        //need to cast variable from double to a string
+//        try {
+//
+//        } catch (java.util.InputMismatchException) {
+//            System.out.println("Please enter a positive integer.");
+//            System.out.println("Enter a radius: ");
+//            radius = input.nextDouble();
+//        }
+        if (radius > 0) {
+            double area = Circle.getArea(radius);
+            System.out.println("The area of a circle of radius " + radius + " is: " + area);
+        }
     }
 }
