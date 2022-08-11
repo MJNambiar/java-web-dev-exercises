de4c2618b309c65df7d8d9b3f5fa861eb4826f8d
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-08-11 11:47:50 -0500

    studio 8.11

--- src/Studio/countingcharacters/CountCharacters.java
+++ src/Studio/countingcharacters/CountCharacters.java
@@ -1,2 +1,33 @@
-package Studio.countingcharacters;public class CountCharacters {
+package Studio.countingcharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountCharacters {
+    public static void main (String[] args) {
+        String statement = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charactersInStatement = statement.toCharArray();
+//        System.out.println(charactersInStatement);
+
+        HashMap<Character, Integer> characterCounts = new HashMap<>();
+        Integer numberTimes = 0;
+        for (char letter : charactersInStatement) {
+
+            if (characterCounts.containsKey(letter)) {
+                numberTimes += 1;
+                characterCounts.put(letter, numberTimes);
+                System.out.println("if clause: " + characterCounts);
+            } else if (!characterCounts.containsKey(letter)) {
+                numberTimes = 1;
+                characterCounts.put(letter, numberTimes);
+                System.out.println("else if clause: " + characterCounts);
+            }
+        }
+
+//        System.out.println(characterCounts);
+        for (Map.Entry<Character, Integer> characterCount : characterCounts.entrySet()) {
+            System.out.println(characterCount.getKey() + ": " + characterCount.getValue());
+        }
+    }
 }
+
